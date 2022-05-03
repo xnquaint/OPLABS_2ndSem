2a9a43f2904de83f7192177156984fe9bafd364c
Stanislav Vdovychenko <89263211+xnquaint@users.noreply.github.com>
2022-05-04 01:06:16 +0300

    Add files via upload

---
+++ classes/Flat.cpp
@@ -0,0 +1,19 @@
+
+#include "Flat.h"
+
+double Flat::get_a() const {
+    return a_coefficient;
+}
+
+double Flat::get_b() const {
+    return b_coefficient;
+}
+
+double Flat::get_c() const {
+    return c_coefficient;
+}
+
+double Flat::get_d() const {
+    return d_coefficient;
+}
+
